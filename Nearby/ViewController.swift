f6bf5ebcfa3b01ac06a0bb0b980c66a064dd63ff
Mateus Coelho <teus.hcp@gmail.com>
2024-11-04 11:55:09 -0300

    Initial Commit

---
+++ Nearby/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nearby
+//
+//  Created by Mateus Henrique Coelho de Paulo on 04/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
